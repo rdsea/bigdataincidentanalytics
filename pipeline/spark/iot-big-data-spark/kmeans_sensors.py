eddafd1e5e8c08728023fbd701a83b7202b90895
Daniel Füvesi <daniel.fuevesi@gmail.com>
2019-12-04 01:21:26 +0100

    PySpark: fix clustering issues, clean up code; Turn off Graphite metrics reporting for the time being; docker-compose.yml: mount the Spark Python code into the container s.t. the image doesn't need to be rebuilt every time the code is changed - this way one can change the code and restart the container with the new code mounted into the PySpark client

--- pipeline/spark/iot-big-data-spark/kmeans_sensors.py
+++ pipeline/spark/iot-big-data-spark/kmeans_sensors.py
@@ -5,13 +5,15 @@ import datetime
 import os
 import json
 import uuid
+import pandas as pd
 
-from pyspark import SparkContext
+from pyspark import SparkContext, RDD
 from pyspark.ml.evaluation import ClusteringEvaluator
 from fluent import asyncsender as sender
 from pyspark.ml.feature import VectorAssembler
 from pyspark.sql.context import SQLContext
 from pyspark.ml.clustering import KMeans
+from pyspark.sql.types import IntegerType, TimestampType
 
 tag = os.environ.get('FLUENTD_TAG_PREFIX', default="spark-kmeans")
 host = os.environ.get('FLUENTD_HOST', default="localhost")
@@ -34,23 +36,35 @@ def process_record(record):
     )
 
 
+def check_data_quality(json_rdd_s: RDD):
+    for member in json_rdd_s.collect():
+        process_record(member)
+
+
 if __name__ == "__main__":
     logger = sender.FluentSender(tag=tag, host=host, port=int(port), nanosecond_precision=True)
     sc = SparkContext(appName="KMeansSensorData",
                       master="spark://" + os.environ.get('SPARK_MASTER_NAME', default="spark-master") + ":"
-                             + os.environ.get('SPARK_MASTER_PORT', default="7077"))  # SparkContext
+                             + os.environ.get('SPARK_MASTER_PORT', default="7077"))
     sqlContext = SQLContext(sc)
-    # Load and parse the data
+
     try:
-        data = sc.wholeTextFiles(hdfsUrl)
-
+        # Load data
         rdd = sc.wholeTextFiles(hdfsUrl)
-        for member in rdd.collect():
-            json_rec = json.loads(member[1])
-            process_record(json_rec)
 
+        # Parse each entry to a valid JSON
         json_rdd = rdd.map(lambda x: json.loads(x[1]))
+
+        # Perform data quality checks, logging of events
+        check_data_quality(json_rdd)
+
+        # Create DataFrame with proper types
         df = sqlContext.read.json(json_rdd)
+        df = df.withColumn("humidity", df["humidity"].cast(IntegerType()))
+        df = df.withColumn("temperature", df["temperature"].cast(IntegerType()))
+        df = df.withColumn("time", df["time"].cast(TimestampType()))
+
+        # Create a target vector for clustering. In this case we combine the humidity and temperature values
         vecAssembler = VectorAssembler(inputCols=["humidity", "temperature"], outputCol="features")
         new_df = vecAssembler.transform(df)
 
@@ -58,9 +72,15 @@ if __name__ == "__main__":
         k_means = KMeans(k=2, seed=1)
         model = k_means.fit(new_df.select('features'))
 
+        # Get the predictions
         predictions = model.transform(new_df)
-        file_name = 'predictions_' + str(datetime.datetime.utcnow().isoformat("T") + "Z") + '.csv'
-        predictions.write.csv('/spark/data/predictions/' + file_name)
+
+        # Save the predictions to a local .CSV file for debugging purposes
+        file_name = 'predictions_' + str(datetime.datetime.utcnow().isoformat("T") + "Z").replace('-', '_')\
+                    .replace(':', '_')\
+                    .replace('.', '_') + '.csv'
+        predictions.sort(["time"]).toPandas().to_csv("/spark/data/predictions/" + file_name, index=False)
+
         logger.emit_with_time('ml.app.dataAsset', time.time(), {
             'specversion': '0.3',
             'id': str(uuid.uuid4()),
@@ -84,10 +104,10 @@ if __name__ == "__main__":
             'time': str(datetime.datetime.utcnow().isoformat("T") + "Z"),
             'source': 'Spark/KMeansSensorData(' + sc.applicationId + ')/cluster_data',
             'subject': 'clustering_analysis_result_' + unique_id,
-            'message': 'Within Set Sum of Squared Error computed',
+            'message': 'KMeans Silhouette Score and Cluster Centers computed (AnalysisId:' + unique_id + ")",
             'data': {
                 'silhouette_score': float(silhouette),
-                'cluster_centers': model.clusterCenters()
+                'cluster_centers': pd.Series(model.clusterCenters()).to_json(orient='values')
             }
         })
 
