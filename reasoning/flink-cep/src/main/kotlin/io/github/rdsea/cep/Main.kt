71b618803e2e899ec4b484272c7881b3f5bced89
Daniel Füvesi <daniel.fuevesi@gmail.com>
2020-01-21 12:52:04 +0100

    Trying out Flink CEP Pattern API

--- reasoning/flink-cep/src/main/kotlin/io/github/rdsea/cep/Main.kt
+++ reasoning/flink-cep/src/main/kotlin/io/github/rdsea/cep/Main.kt
@@ -1,5 +1,14 @@
 package io.github.rdsea.cep
 
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.api.datastream.DataStream
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+import java.lang.IllegalArgumentException
+import java.util.Properties
+
 /**
  * <h4>About this class</h4>
  *
@@ -12,9 +21,31 @@ package io.github.rdsea.cep
 class Main {
 
     companion object {
-        @JvmStatic fun main(args: Array<String>) {
+        private val gson = Gson()
+        private val typeToken = object : TypeToken<Map<String, Any>>() {}.type
 
+        @JvmStatic fun main(args: Array<String>) {
+            checkArgs(args)
+
+            val env = StreamExecutionEnvironment.getExecutionEnvironment()
+
+            val properties = Properties()
+            properties.setProperty("bootstrap.servers", args[0])
+            properties.setProperty("group.id", args[1])
+            val dataStream: DataStream<Map<String, Any>> = env
+                .addSource(FlinkKafkaConsumer("signals", SimpleStringSchema(), properties))
+                .map {
+                    val json: Map<String, Any> = gson.fromJson(it, typeToken)
+                    json
+                }
+
+            env.execute("Complex Event Processing")
         }
 
+        private fun checkArgs(args: Array<String>) {
+            if (args.size != 2) {
+                throw IllegalArgumentException("There must be exactly 2 arguments: <BROKERS> <GROUP_ID>")
+            }
+        }
     }
 }
