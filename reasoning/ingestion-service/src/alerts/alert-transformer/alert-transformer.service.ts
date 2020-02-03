43a81f6ea97ddb05f762f66512fb43be5584486e
Daniel Füvesi <daniel.fuevesi@gmail.com>
2020-02-03 22:25:32 +0100

    fix bug in alert-transformer.service.ts where the outdated PrometheusAlertDto was present from the dist instead of using the new PromethesAlert one; add sample json and reamde to .dockerignore

--- reasoning/ingestion-service/src/alerts/alert-transformer/alert-transformer.service.ts
+++ reasoning/ingestion-service/src/alerts/alert-transformer/alert-transformer.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import {PrometheusAlertDto} from "../../../dist/alerts/dto/prometheus-alert-dto";
 import {TransformedPrometheusAlert} from "../dto/transformed-prometheus-alert";
+import {PrometheusAlert} from "../dto/prometheus-alert";
 
 @Injectable()
 export class AlertTransformerService {
     private signalType = "PROMETHEUS_ALERT";
 
-    async transform(original: PrometheusAlertDto): Promise<TransformedPrometheusAlert> {
+    async transform(original: PrometheusAlert): Promise<TransformedPrometheusAlert> {
         return new Promise(resolve => resolve(new TransformedPrometheusAlert(original, this.signalType)));
     }
 }
